36db25ea19ee07c9b953999ba1aad749b1157e60
developer abir <developerabir40@gmail.com>
2022-08-14 08:15:21 +0600

    amazona

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import '../styles/globals.css';
 import { StoreProvider } from '../utils/store';
-
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 function MyApp({ Component, pageProps }) {
--- pages/login.jsx
+++ pages/login.jsx
@@ -1,45 +1,77 @@
 import Link from 'next/link';
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import Layout from '../components/Layout';
 
 export default function LoginScreen() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const submitHandle = ({ email, password }) => {
+    console.log(email, password);
+  };
+
   return (
     <Layout title={'Login'}>
-      <from className="mx-auto  max-w-screen-md container">
-        <h1 className="mb-4 text-xl"> Login</h1>
-        <div className="mb-4">
+      <div className=" w-1/2 mx-auto mt-12">
+        <h1 className="mb-4 text-3xl text-center   "> Login</h1>
+
+        <form onSubmit={handleSubmit(submitHandle)}>
           <div className="mb-4">
-            <label htmlFor="email ">Email</label>
+            <label className="label">
+              <span className="label-text">Email Address?</span>
+            </label>
             <input
               type="email"
-              placeholder="Enter Email address"
-              className="input input-bordered w-full "
-              id="email"
-              autoFocus
+              placeholder="Enter Email"
+              className="input input-bordered input-warning w-full  "
+              {...register('email', {
+                required: 'Please enter email address',
+                pattern: {
+                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                  message: 'Enter valid email address',
+                },
+              })}
             />
+
+            {errors.email && (
+              <div className="text-red-400 my-2">{errors.email.message}</div>
+            )}
           </div>
-          <div className="mb-4">
-            <label htmlFor="password ">Password</label>
+          <div className="mb-3">
+            <label className="label">
+              <span className="label-text">Enter password</span>
+            </label>
             <input
               type="password"
-              placeholder="Enter password *"
-              className="input input-bordered w-full  "
-              id="password"
-              autoFocus
+              placeholder="Enter password"
+              className="input input-bordered input-warning w-full  "
+              {...register('password', {
+                required: 'Please Enter passwrod',
+                minLength: {
+                  value: 6,
+                  message: 'password is more then 5 char',
+                },
+              })}
             />
+
+            {errors.password && (
+              <div className="text-red-400 my-2">{errors.password.message}</div>
+            )}
           </div>
 
-          <div className="mb-4">
-            <button className="primary-button  ">Login</button>
-          </div>
-          <div className="mb-4">
-            <span>Don&apos;t have an account?</span>
-            <Link href={'/register'}>
-              <span className="underline">&nbsp; Register</span>
-            </Link>
-          </div>
+          <input type="submit" className="primary-button" />
+        </form>
+        <div className="mb-4">
+          <span>Don&apos;t have an account?</span>
+          <Link href={'/register'}>
+            <span className="underline">&nbsp; Register</span>
+          </Link>
         </div>
-      </from>
+      </div>
     </Layout>
   );
 }
